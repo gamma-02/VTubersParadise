6e309c9cf869075fa7bbd62275af391ed63a46d4
gamma_02 <gammagames06@gmail.com>
2021-12-02 15:40:25 -0800

    finished

--- src/main/java/gamma02/vtubersparadise/items/NeonBlade/NeonBladeL1.java
+++ src/main/java/gamma02/vtubersparadise/items/NeonBlade/NeonBladeL1.java
@@ -1,8 +1,16 @@
 package gamma02.vtubersparadise.items.NeonBlade;
 
 import gamma02.vtubersparadise.items.ISweepingNoEnchant;
+import net.minecraft.enchantment.SweepingEnchantment;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 
 public class NeonBladeL1 extends SwordItem implements ISweepingNoEnchant
 {
@@ -12,6 +20,26 @@ public class NeonBladeL1 extends SwordItem implements ISweepingNoEnchant
         super(tier, attackDamageIn, attackSpeedIn, builderIn);
     }
 
+    @Override public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker)
+    {
+        for (LivingEntity livingentity : target.world.getEntitiesWithinAABB(LivingEntity.class,
+                target.getBoundingBox().grow(1.0D, 0.25D, 1.0D)))
+        {
+            if (livingentity != attacker && livingentity != target && (!(livingentity instanceof ArmorStandEntity) || !((ArmorStandEntity) livingentity).hasMarker()) && attacker.getDistanceSq(
+                    livingentity) < 9.0D)
+            {
+                livingentity.applyKnockback(0.4F, (double) MathHelper.sin(attacker.rotationYaw * ((float) Math.PI / 180F)),
+                        (double) (-MathHelper.cos(attacker.rotationYaw * ((float) Math.PI / 180F))));
 
+                livingentity.attackEntityFrom(DamageSource.causeMobDamage(attacker), SweepingEnchantment.getSweepingDamageRatio(10));
+            }
+        }
+        attacker.world.playSound((PlayerEntity)null, attacker.getPosX(), attacker.getPosY(), attacker.getPosZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, attacker.getSoundCategory(), 1.0F, 1.0F);
+        if(attacker instanceof PlayerEntity){
+            PlayerEntity user = (PlayerEntity) attacker;
+            user.spawnSweepParticles();
+        }
+        return super.hitEntity(stack, target, attacker);
 
+    }
 }
--- src/main/java/gamma02/vtubersparadise/items/ShineSword/ShineSwordL1.java
+++ src/main/java/gamma02/vtubersparadise/items/ShineSword/ShineSwordL1.java
@@ -1,12 +1,18 @@
 package gamma02.vtubersparadise.items.ShineSword;
 
 import gamma02.vtubersparadise.items.ISweepingNoEnchant;
+import net.minecraft.enchantment.SweepingEnchantment;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
 
 public class ShineSwordL1 extends SwordItem implements ISweepingNoEnchant
@@ -19,6 +25,24 @@ public class ShineSwordL1 extends SwordItem implements ISweepingNoEnchant
     @Override public boolean hitEntity(@NotNull ItemStack stack, LivingEntity target, @NotNull LivingEntity attacker)
     {
         target.addPotionEffect(new EffectInstance(Effects.GLOWING, 600));
+
+        for (LivingEntity livingentity : target.world.getEntitiesWithinAABB(LivingEntity.class,
+                target.getBoundingBox().grow(1.0D, 0.25D, 1.0D)))
+        {
+            if (livingentity != attacker && livingentity != target && (!(livingentity instanceof ArmorStandEntity) || !((ArmorStandEntity) livingentity).hasMarker()) && attacker.getDistanceSq(
+                    livingentity) < 9.0D)
+            {
+                livingentity.applyKnockback(0.4F, (double) MathHelper.sin(attacker.rotationYaw * ((float) Math.PI / 180F)),
+                        (double) (-MathHelper.cos(attacker.rotationYaw * ((float) Math.PI / 180F))));
+
+                livingentity.attackEntityFrom(DamageSource.causeMobDamage(attacker), SweepingEnchantment.getSweepingDamageRatio(10));
+            }
+        }
+        attacker.world.playSound((PlayerEntity)null, attacker.getPosX(), attacker.getPosY(), attacker.getPosZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, attacker.getSoundCategory(), 1.0F, 1.0F);
+        if(attacker instanceof PlayerEntity){
+            PlayerEntity user = (PlayerEntity) attacker;
+            user.spawnSweepParticles();
+        }
         return super.hitEntity(stack, target, attacker);
     }
 }
